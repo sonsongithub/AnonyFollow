6eb1a886e6e69d2875de202536bc56562e9e468c
sonson <yoshida.yuichi@gmail.com>
2012-08-27 00:29:12 +0900

    アイコン画像のキャッシュ機能追加．

--- AnonyFollow/TwitterAccountInfo.m
+++ AnonyFollow/TwitterAccountInfo.m
@@ -12,7 +12,37 @@
 
 #import "AccountCell.h"
 
+#import "NSData+MD5.h"
+
 @implementation TwitterAccountInfo
+
+#pragma mark - Manages cache of thumbnails and sssp images.
+
++ (UIImage*)cacheImageWithURLString:(NSString*)URLString {
+	
+	NSString *hash = [URLString md5HexHash];
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+	NSString *cacheDirectory = [paths objectAtIndex:0];
+	NSString *thumbnailCacheDirectory = [cacheDirectory stringByAppendingPathComponent:@"icon_image"];
+	
+	NSString *cacheFilePath = [thumbnailCacheDirectory stringByAppendingPathComponent:hash];
+	
+	return [UIImage imageWithContentsOfFile:cacheFilePath];
+}
+
++ (BOOL)writeCacheImageWithData:(NSData*)data URLString:(NSString*)URLString {
+	
+	NSString *hash = [URLString md5HexHash];
+	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+	NSString *cacheDirectory = [paths objectAtIndex:0];
+	NSString *thumbnailCacheDirectory = [cacheDirectory stringByAppendingPathComponent:@"icon_image"];
+	
+	[[NSFileManager defaultManager] createDirectoryAtPath:thumbnailCacheDirectory withIntermediateDirectories:YES attributes:nil error:nil];
+	
+	NSString *cacheFilePath = [thumbnailCacheDirectory stringByAppendingPathComponent:hash];
+	
+	return [data writeToFile:cacheFilePath atomically:NO];
+}
 
 - (DownloadTask*)taskForUserTimeline {
 	NSString *URLString = [NSString stringWithFormat:@"https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=%@", self.screenName];
@@ -22,13 +52,22 @@
 }
 
 - (BOOL)tryToDownloadIconImage {
+	NSString *URLString = [NSString stringWithFormat:@"https://api.twitter.com/1/users/profile_image?screen_name=%@&size=bigger", self.screenName];
+	
+	UIImage *cache = [TwitterAccountInfo cacheImageWithURLString:URLString];
+	
+	if (cache) {
+		self.iconImage = cache;
+		[[NSNotificationCenter defaultCenter] postNotificationName:AccountCellUpdateNotification object:nil userInfo:nil];
+		return NO;
+	}
 	
 	if (self.iconImage)
 		return NO;
 	
 	[[DownloadQueue sharedInstance] removeTasksOfDelegate:self];
 	
-	NSString *URLString = [NSString stringWithFormat:@"https://api.twitter.com/1/users/profile_image?screen_name=%@&size=bigger", self.screenName];
+	
 	DownloadTask *task = [[DownloadTask alloc] init];
 	task.request = [NSURLRequest requestWithURL:[NSURL URLWithString:URLString]];
 	task.delegate = self;
@@ -41,6 +80,7 @@
 	UIImage *image = [UIImage imageWithData:task.data];
 	self.iconImage = image;
 	DNSLog(@"%@", image);
+	[TwitterAccountInfo writeCacheImageWithData:task.data URLString:[task.request.URL absoluteString]];
 	[[NSNotificationCenter defaultCenter] postNotificationName:AccountCellUpdateNotification object:nil userInfo:nil];
 }
 
